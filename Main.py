d1da1ae21dcae0dcf68a34d8e615649c0be820aa
Humberto Tovar <133364869+Atovar18@users.noreply.github.com>
2024-07-25 13:23:54 -0400

    Matriz de incidencia lista

--- Main.py
+++ Main.py
@@ -1,4 +1,8 @@
 from Modulos import Lectura
+import numpy as np
+import pandas as pd
+import itertools
+from Modulos import Y_bus
 
 # ================================================================================ Lectura de los datos. ================================================================================================================
 GS, NR, FD, DC, Convergencia, Max_iter = Lectura.CONFIG ()
@@ -14,10 +18,23 @@ ID_SHUNT, Bus_i_shunt, R_Shunt, X_Shunt = Lectura.SHUNT_ELEMENTS ()
 # ============================================================================= Comprobación de las barras. =============================================================================================================
 Lectura.COMPROBACION (Barra_i, Bus_i_lineas, Bus_j_lineas, Bus_i_trx, Bus_j_trx, Bus_i_shunt)
 
-# ============================================================================= Creación de las matrices Y_rama. ========================================================================================================
+MatrizA, elementos_a_tierra_arr, Barra_i_conex, Barra_j_conex, Y_linea, TRX_I, TRX_J, Tomas_a_tierra, SeriesTRX = Y_bus.Incidencia_Nodal(Barra_i, Bus_i_lineas, Bus_j_lineas, R_lineas, X_lineas, B_lineas, Bus_i_trx, Bus_j_trx, Xcc_trx, Tap_trx, Bus_i_shunt)
 
 
-
-
-
-
+print (MatrizA)
+print ()
+print (elementos_a_tierra_arr)
+print ()
+print (Barra_i_conex)
+print ()
+print (Barra_j_conex)
+print ()
+print (Y_linea)
+print ()
+print (TRX_I)
+print ()
+print (TRX_J)
+print ()
+print (Tomas_a_tierra)
+print ()
+print (SeriesTRX)
